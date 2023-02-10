a9e4b5e252483262a78e047c50a5e5ce926b8eca
Andrea F <andrea.fabbricatore9569@gmail.com>
2023-02-10 12:55:26 +0100

    10/02 validate email

---
+++ form_in_php/class/validator/ValidateMail.php
@@ -0,0 +1,8 @@
+<?php
+
+class validateMail{
+    public function isValid (string $email): bool {
+       return filter_var($email, FILTER_VALIDATE_EMAIL);
+    }
+
+}
---
+++ form_in_php/test/test_valutazione_email.php
@@ -0,0 +1,26 @@
+<?php
+//$files = scandir(".");
+//print_r($files);
+
+require "./form_in_php/class/validator/ValidateMail.php";
+
+$emails = [
+    'a',
+    '   ',
+    'a@',
+    'mario',
+];
+
+$v = new ValidateMail();
+
+//v.isValid('a')
+
+foreach ($emails as $email) {
+
+    if($v->isValid($email)== false) {
+        echo "test superato per $email\n";
+    }else{
+        echo"test numero $index non superato per [$email]\n";
+    };
+    //$v->getMessage();
+}
