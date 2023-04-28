1b5f601428dcf90df915fb573071ebca723cc4b2
Andrea Fabbricatore <118559209+Morgana9569@users.noreply.github.com>
2023-04-28 12:24:10 +0200

    Create TaskItem.js

---
+++ form_in_php/__react-app/todo-list/src/components/TaskItem/TaskItem.js
@@ -0,0 +1,36 @@
+// const { nome_task, done }= props
+// props.done, props.nome_task
+
+import { useState } from "react"
+
+
+function TaskItem({ nome_task, done,id,parentRemoveTask }){
+    const [doneCheckbox,setDoneCheckbox]=useState(done)
+
+    function onRemoveTask(){
+            console.log("task "+id)
+            parentRemoveTask(id)
+    }
+
+    function onUpdateStatus(event) {
+            
+        
+    }
+
+    return (
+        <li className={done ? 'task_gia_fatta': ''}>
+            {doneCheckbox ? <h1>FATTO</h1> :  <h1>DA FARE</h1> }
+            <input onChange={(event)=>setDoneCheckbox(event.target.checked)}
+                   checked={doneCheckbox}
+                  
+                   type="checkbox" />
+             {done}
+            <label>{ nome_task }</label>
+            <input type="text" defaultValue="Go Siihopping" />
+            <button className="edit">Edit</button>
+            <button className="delete" onClick={onRemoveTask} >Delete</button>
+      </li>
+    )
+}
+
+export default TaskItem;
